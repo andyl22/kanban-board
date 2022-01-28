edcaec5382814f8df0d508bb708d2230e5573cc2
Andy Lau <landywl4@gmail.com>
2022-01-28 00:06:00 -0800

    heroku

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const proxyTarget =
   process.env.NODE_ENV !== "production"
     ? "http://backend:3001"
-    : "http://localhost:3001";
+    : "https://andyl22-kanban-board.herokuapp.com";
 
 module.exports = function (app) {
   app.use(
